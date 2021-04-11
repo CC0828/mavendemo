57f6d243d8e7be49123f7b2a6f5ffddca704e974
chencong <1209321433@qq.com>
2021-04-11 16:42:23 +0800

    Initial commit

---
+++ src/main/java/com/offcn/test/Main.java
@@ -0,0 +1,14 @@
+package com.offcn.test;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add("A");
+        linkedList.add(1,"B");
+        System.out.println(linkedList.get(1));
+    }
+
+}
