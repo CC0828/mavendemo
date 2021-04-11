24c62562d92347a147d9b8a7060001121cf17337
chencong <1209321433@qq.com>
2021-04-11 17:40:08 +0800

    第二次33

--- src/main/java/com/offcn/test/Main.java
+++ src/main/java/com/offcn/test/Main.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-        linkedList.add("A");
-        linkedList.add(1,"B");
-        System.out.println(linkedList.get(1));
+
+     
         System.out.println("12345");
         System.out.println("abcde");
-        System.out.println("fdsafdsafa");
+        System.out.println("fdsafdsa5fa");
         System.out.println("fdsafdsaafdsafdsafads");
         System.out.println("fdsafdsaafdsafdsafads0");
     }
